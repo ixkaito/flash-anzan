00757a783a96b56f51d9d662d2a588065f03a7f8
Kite <ixkaito@gmail.com>
2016-02-27 22:04:19 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+var max = 10;
+var question = document.getElementById('js-question');
+var solution = document.getElementById('js-solution');
+var answer, i, timer, rand;
+solution.value = '';
+
+function start() {
+  answer = 0;
+  i = 0;
+  solution.value = '';
+  flash();
+}
+
+function flash() {
+  rand = Math.floor((Math.random() * 99) + 1);
+  if (i < max) {
+    timer = setTimeout(function() {
+      question.innerHTML = rand;
+      answer += rand;
+      setTimeout(function() {
+        question.innerHTML = '';
+      }, 700);
+      flash();
+    }, 1000);
+    i++;
+  } else {
+    clearTimeout(timer);
+  }
+}
+
+function verify() {
+  if (solution.value == answer) {
+    alert('正解！');
+  } else if (answer) {
+    alert('残念！不正解...');
+  }
+}
+
+function showanswer() {
+  alert('正解は ' + answer + ' です。');
+}
